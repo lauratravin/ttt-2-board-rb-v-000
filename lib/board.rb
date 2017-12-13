4eedeb2ca5405ff4ab6842e287f381c8e15aa629
Laura M. Travin <laura.travin@gmail.com>
2017-12-13 23:52:03 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,7 @@
 # Define the variable board below.
 board = Array.new(9)
-i = 0
-9.time do
-  board[i]  =   ""
- i = i + 1
 
+9.time do
+  board.push("")
 end
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
