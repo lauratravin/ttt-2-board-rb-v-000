b69bb7d18b764b80cf92afe5e1606f3c3293e951
Laura M. Travin <laura.travin@gmail.com>
2017-12-13 23:54:19 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 # Define the variable board below.
-board = Array.new(0)
+board = Array.new(0) #create an empty array
 
 9.times do
-  board.push(" ")
+  board.push(" ") #add elements to array.
 end
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
